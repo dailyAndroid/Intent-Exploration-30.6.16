daee96d43c73ea00cc43051f476a14e5abcac37a
dailyAndroid <hwhong1129@gmail.com>
2016-08-16 14:51:10 +0800

    initial commit

---
+++ app/src/main/java/warrenhong/intentexploration/Main2Activity.java
@@ -0,0 +1,22 @@
+package warrenhong.intentexploration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("NAME_OF_USER");
+
+        textView.setText("Hello, " + name);
+    }
+}
---
+++ app/src/main/java/warrenhong/intentexploration/MainActivity.java
@@ -0,0 +1,37 @@
+package warrenhong.intentexploration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    final static String NAME_OF_USER = "NAME_OF_USER";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                intent.putExtra(NAME_OF_USER, name);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
